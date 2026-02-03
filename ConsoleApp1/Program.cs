bf56d0cb971a3032796b2234bd3bf446b40ebdab
MostafaRamadan27 <mostafaramadan200018@gmail.com>
2026-02-03 23:11:49 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,84 @@
+﻿using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //q1
+            //StringBuilder Name  = new StringBuilder("hello");
+            //for (int i = Name.Length -1 ; i >= 0 ; i--)
+            //{
+            //    Console.Write($"{Name[i]}");
+            //}
+
+            //q2
+
+            //for (int i = 0; i < 50; i++)
+            //{
+            //    if (i % 3 == 0 && i % 5 == 0)
+            //    {
+            //        Console.WriteLine("Fizz");
+            //    }
+            //    else if(i % 5 == 0)
+            //    {
+            //        Console.WriteLine("Buzz");
+            //    }
+            //    else if (i % 3 == 0 )
+            //    {
+            //        Console.WriteLine("FizzBuzz");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+
+
+            //}
+
+
+
+
+
+
+            //q3
+
+            //Console.WriteLine("Enter Degree");
+            //int grade;
+            //int.TryParse(Console.ReadLine() , out grade);
+
+            //switch (grade)
+            //{
+            //    case >= 90:
+            //        Console.WriteLine("A");
+            //        break;
+            //    case >= 80:
+            //        Console.WriteLine("B");
+            //        break;
+            //    case >= 70:
+            //        Console.WriteLine("C");
+            //        break;
+            //    case >= 60:
+            //        Console.WriteLine("D");
+            //        break;
+            //    case < 60:
+            //        Console.WriteLine("F");
+            //        break;
+            //    default:
+            //        Console.WriteLine("invalid degree");
+            //        break;
+            //}
+
+            //q4
+
+          
+
+
+
+
+
+
+        }
+    }
+}
